4735d079cf8331db1788ff65b6fbf47c7c7bbe2d
Rafał Szopa <rafal.szopa@op.pl>
2022-02-10 00:22:55 +0100

    js/script.js, add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -1,28 +1,23 @@
-/*
-document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  });
-  */
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
+    console.log('Link was clicked!', event);
   
-    /* [DONE] remove class 'active' from all article links  */
+    /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    console.log(activeLinks);
+    console.log('Aktywny link:',activeLinks);
 
-    /* [IN PROGRES] add class 'active' to the clicked link */
+    /* add class 'active' to the clicked link */
     this.classList.add('active')
 
     console.log('clickedElement:', clickedElement);
 
-    /* [DONE] remove class 'active' from all articles */
+    /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
@@ -30,10 +25,15 @@ document.getElementById('test-button').addEventListener('click', function(){
     }
 
     /* get 'href' attribute from the clicked link */
-  
+    const articleSelector = this.getAttribute('href')
+    console.log(articleSelector)
+
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle = document.querySelector(articleSelector)
+    console.log(targetArticle)
+
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active')
   }
   
   const links = document.querySelectorAll('.titles a');
